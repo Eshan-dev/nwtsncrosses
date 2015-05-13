a378252515d8669428dba3f1f38f8c5ff9819964
Jim Banks <iamjimhbanks@me.com>
2015-05-13 18:31:14 +0100

    plays now stored in array

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,38 @@
 $(document).ready(function() {
    
+  var player = ''
+  var winSeq = [[1, 2, 3], [1, 4, 7], [2, 5, 8],[3, 6, 9], [4, 5, 6], [7, 8, 9], [3, 5, 7]];
 
-  // var winSeq ['1', '2', '3'],['1', '4', '7'],['2', '5', '8'],['3', '6', '9'],['4', '5', '6'],['7', '8', '9'],['7', '5', '3'];
-
-  // var start = "o";
-  // var xTurn = ['xTurn'];
-  // var oTurn = [];
   var thisTurn = 'xTurn';
   var grid = ['','','','','','','','',''];
 
   $('.playerInput').on('click', function(){
+    var id= Number($(this).attr('id'));
     if (thisTurn === 'xTurn') {
-      this.textContent='x'; 
-      console.log(this);
-      thisTurn = 'oTurn';
+        player = 'x'
+        this.textContent= player; 
+        thisTurn = 'oTurn';
+        grid[id -1] = 'x'  
+      console.log(grid) 
     } else {
-      this.textContent='o'; 
-      thisTurn ='xTurn'
+        player = 'o'
+        this.textContent= player; 
+        thisTurn ='xTurn'
+        grid[id -1] = 'o'
+      console.log(grid) 
+
     }
   });
+ 
+
+
+
+ function playerWin {
+  $.this(winSeq === true)
+ }
+
+
+
 });
 
 
