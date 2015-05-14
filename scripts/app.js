dd3f290d65f20fa493103fb4dcc2e642ab114c88
Jim Banks <iamjimhbanks@me.com>
2015-05-14 10:54:37 +0100

    started on win function

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
+
+//   soundManager.setup({
+//  url: '../audio/',
+//  onready: function() {
+
+
+//  $('#makeSound').on('click', function(){
+//      var mySound = soundManager.createSound({
+//        url: 'audio/starwarsthemenew.mp3'
+//      });
+//      mySound.play();
+//    });
+
+// },
+//  ontimeout: function() {
+//  }
+
    
   var player = ''
   var winSeq = [[1, 2, 3], [1, 4, 7], [2, 5, 8],[3, 6, 9], [4, 5, 6], [7, 8, 9], [3, 5, 7]];
-
   var thisTurn = 'xTurn';
   var grid = ['','','','','','','','',''];
+
 
   $('.playerInput').on('click', function(){
     var id= Number($(this).attr('id'));
@@ -21,39 +38,32 @@ $(document).ready(function() {
         grid[id -1] = 'o'
       console.log(grid) 
 
-    }
-  });
-});
-
-
-
-//  function playerWin {
-//   $.this(winSeq === true)
-//  }
-
-
-
-
-
-
-soundManager.setup({
- url: '../audiofiles/',
- onready: function() {
-
-
- $('#intro').on('click', function(){
-     var mySound = soundManager.createSound({
-       url: 'audio/starwarsthemenew.mp3'
-     });
-     mySound.play();
+     }
    });
+  });
 
-},
- ontimeout: function() {
- }
+function checkWin(winSeq)
+var playArray === winSeq.sort()
 
+// fucntion checkWin, cehck that against player
+// 2x array 1x x move 1 x o moves nd store moves 
+// winning combo array
+//wining grid array
+// mxoves [ 0.1,2 etc]
 
+  // $('.playerInput').on('click', function(){
+  // var id= Number($(this).attr('id'));
+  // if (thisTurn === winSeq) {
+  //   } else {
+  //       thisTurn === winSeq
+  //     console.log(winSeq) 
+
+  //  });
  
+
+
+
+
  
     
 
