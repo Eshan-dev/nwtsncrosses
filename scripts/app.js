4c0c75c2611c85f1702f4eab518234a264b5ed3b
Jim Banks <iamjimhbanks@me.com>
2015-05-14 15:20:15 +0100

    added sounds

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
-
-//   soundManager.setup({
-//  url: '../audio/',
-//  onready: function() {
-
-
-//  $('#makeSound').on('click', function(){
-//      var mySound = soundManager.createSound({
-//        url: 'audio/starwarsthemenew.mp3'
-//      });
-//      mySound.play();
-//    });
-
-// },
-//  ontimeout: function() {
-//  }
-
    
   var player = ''
   var winSeq = [[0, 1, 2], [0, 3, 6], [1, 4, 7],[2, 5, 8], [3, 4, 5], [6, 7, 8], [2, 4, 6]];
@@ -39,25 +22,47 @@ $(document).ready(function() {
         grid[id -1] = 'o'
         checkWin(player)
       // console.log(grid) 
-
      }
    });
 function checkWin(player){
-
-    
     for(i = 0; i < winSeq.length; i++) {
       for (s = 0; s < grid.length; s++) {
         if (grid[s] === player){
-          if(winSeq[i][] === s){
-            console.log('matches' + winSeq[i])
+          if(winSeq[i] === grid[s][0]){
+            console.log('matches')
           }
-
         }
       }
     }
   } 
+
+
+soundManager.setup({
+url: '../audio/',
+onready: function() {
+
+   $('.playerInput').on('click', function(){
+     var mySound = soundManager.createSound({
+       url: 'audio/Lightsaber Clash-SoundBible.com-203518049.mp3'
+     });
+     mySound.play();
+   });
+
+
+
+ var mySound = soundManager.createSound({
+  url: 'audio/starwarsthemenew.mp3',
+  autoPlay: true,
+  loops: 4,
+  pan: -75,
+  volume: 25
 });
 
+},
+ ontimeout: function() {
+ }
+ });
+  });
  
 //need to push player moves into array to check if winning array
 // track moves by player
